b9666702d149ff2a26977f9b60885c63c59916c4
Allewia Thomas <162132890+allewiathomas@users.noreply.github.com>
2026-04-18 16:35:19 +0530

    Add files via upload

---
+++ allewia-s-data-showcase-main/src/pages/Index.tsx
@@ -0,0 +1,23 @@
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import SkillsSection from "@/components/SkillsSection";
+import ToolsSection from "@/components/ToolsSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <HeroSection />
+      <AboutSection />
+      <SkillsSection />
+      <ToolsSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
